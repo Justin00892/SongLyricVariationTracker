9b1052a8fad759ca6e4481a28d976f761dcb0f6a
Justin.McCarthy <Justin.McCarthy@e-hps.com>
2018-06-20 15:38:16 -0400

    Add project files.

---
+++ WordVariationTracker/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WordVariationTracker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.textLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(12, 12);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.selectFileButton.TabIndex = 0;
+            this.selectFileButton.Text = "Select Files";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // textLabel
+            // 
+            this.textLabel.AutoSize = true;
+            this.textLabel.Location = new System.Drawing.Point(13, 43);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(0, 13);
+            this.textLabel.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.AutoSize = true;
+            this.displayLabel.Location = new System.Drawing.Point(19, 43);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(67, 13);
+            this.displayLabel.TabIndex = 2;
+            this.displayLabel.Text = "Word Count:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.displayLabel);
+            this.Controls.Add(this.textLabel);
+            this.Controls.Add(this.selectFileButton);
+            this.Name = "Word Variation Tracker";
+            this.Text = "Word Variation Tracker";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label displayLabel;
+    }
+}
---
+++ WordVariationTracker/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordVariationTracker
+{
+    public partial class MainForm : Form
+    {
+        private SortedDictionary<string, int> _wordCount = new SortedDictionary<string, int>();
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeOpenFileDialog();
+        }
+
+        private void InitializeOpenFileDialog()
+        {
+            openFileDialog.Filter =
+                "Text (*.txt) |*.txt";
+
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Open Text Files";
+        }
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            var dr = openFileDialog.ShowDialog();
+            if (dr != DialogResult.OK) return;
+            foreach (var file in openFileDialog.FileNames)
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var text = reader.ReadToEnd().ToLower();
+                        var list = text.Split(" ,!.?:;'\"".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                        ProcessList(list);
+                        displayLabel.Text = "Word Count:\n";
+                        foreach (var pair in _wordCount)
+                        {
+                            displayLabel.Text += pair.Key + ": " + pair.Value + "\n";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ProcessList(string[] list)
+        {
+            foreach (var word in list)
+            {
+                var count = 1;
+                if (_wordCount.ContainsKey(word))
+                    count = _wordCount[word] + 1;
+                _wordCount[word] = count;
+            }
+            _wordCount.OrderBy()
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
